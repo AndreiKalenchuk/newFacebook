53988c917bf66ae45f801a368e195b0fbcb3e6e6
KALIANCHUK, ANDREI <ak276p@us.att.com>
2021-02-08 22:14:44 -0500

    separating BLL and UI move data from component to crs preparing to redux

--- src/components/pages/Dialogs/Dialogs.jsx
+++ src/components/pages/Dialogs/Dialogs.jsx
@@ -3,33 +3,14 @@ import css from './Dialogs.module.css';
 import DialogItem from "./DialogItem/DialogItem";
 import Messages from "./messages/Messages";
 
-
 const Dialogs = (props) => {
-    // data:
-    const usersList = [
-        {id: 1, name: 'SASA'},
-        {id: 2, name: 'Andrey'},
-        {id: 3, name: 'Vasily'},
-        {id: 4, name: 'Ana'},
-        {id: 5, name: 'Bedlla'},
-    ];
-    const messagesList = [
-        {id: 1, message: 'Yoou'},
-        {id: 2, message: 'What is up?'},
-        {id: 3, message: 'What ?'},
-        {id: 4, message: 'Go'},
-        {id: 4, message: 'Go home'},
-    ];
-
-    const dialogsItems = usersList.map(dialog => <DialogItem name={dialog.name} id={dialog.id}/>);
-    const messagesItems = messagesList.map(message => <Messages msg={message.message} id={message.id}/>);
-
+    const dialogsItems = props.state.dialogs.map(dialog => <DialogItem name={dialog.name} id={dialog.id}/>);
+    const messagesItems = props.state.messages.map(message => <Messages msg={message.message} id={message.id}/>);
     return (
         <div className={css.dialogs_box}>
             <div className={css.dialogs_items}>
                 {dialogsItems}
             </div>
-
             <div>
                 {messagesItems}
             </div>
--- src/components/pages/Profile/MyPosts/MyPosts.jsx
+++ src/components/pages/Profile/MyPosts/MyPosts.jsx
@@ -2,14 +2,9 @@ import React from 'react';
 import css from './MyPosts.module.css'
 import Post from "./Post/Post";
 
+
 const MyPosts = (props) => {
-    const postsList = [
-        {id: 1, msg: 'Hello, how are you been', likesCount: 2},
-        {id: 2, msg: 'Pass a hammer to me, please', likesCount: 24},
-        {id: 3, msg: 'Hello, I\'m Ok!', likesCount: 25},
-        {id: 4, msg: 'Hello, initial commit', likesCount: 12}
-    ];
-    const postsItems = postsList.map(post => <Post message={post.msg} id={post.id} likesCount={post.likesCount}/>);
+    const postsItems = props.posts.map(post => <Post message={post.msg} id={post.id} likesCount={post.likesCount}/>);
 
     return (
         <div className={css.content}>
