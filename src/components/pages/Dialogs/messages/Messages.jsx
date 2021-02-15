62a7663fabe861c2043daeb51987870b675a664b
KALIANCHUK, ANDREI <ak276p@us.att.com>
2021-02-14 22:34:41 -0500

    synchronization BLL on textArea change and re-render updates

--- src/components/pages/Dialogs/messages/Messages.jsx
+++ src/components/pages/Dialogs/messages/Messages.jsx
@@ -2,9 +2,35 @@ import React from "react";
 import css from './Messages.module.css'
 
 const Messages = (props) => {
-    return(
+
+    const newElement = React.createRef();
+
+    const sendMessage = () => {
+        const text = newElement.current.value;
+        alert(text)
+    };
+
+    return (
         <div className={css.messages}>
+            <div className={css.dialogWrapper}>
+            <span>
+
             {props.msg}
+            </span>
+
+                <span>
+
+                </span>
+
+
+
+
+                <button className={css.submitButton}
+                        onClick={sendMessage}>
+                    Send
+                </button>
+
+            </div>
         </div>
     )
 }
--- src/components/pages/Profile/MyPosts/MyPosts.jsx
+++ src/components/pages/Profile/MyPosts/MyPosts.jsx
@@ -6,18 +6,31 @@ import Post from "./Post/Post";
 const MyPosts = (props) => {
     const postsItems = props.posts.map(post => <Post message={post.msg} id={post.id} likesCount={post.likesCount}/>);
 
+    const newElement = React.createRef();
+
+    const addNewPost = () => {
+        props.addPost();
+        props.updateNewPostText('');
+    };
+
+    const onPostChange = () => {
+        props.updateNewPostText(newElement.current.value);
+    };
+
     return (
         <div className={css.content}>
             <div>
                 <h3>My posts</h3>
                 <div>
                     <div>
-                    <textarea placeholder='type a message'
-                              name="text">
-                    </textarea>
+                    <textarea onChange={onPostChange} ref={newElement}
+                              placedholder='type a message'
+                              value={props.newPostText}/>
                     </div>
                     <div>
-                        <button className={css.submitBtn}> Add post</button>
+                        <button className={css.submitBtn}
+                                onClick={addNewPost}> Add post
+                        </button>
                     </div>
                 </div>
                 <div>
