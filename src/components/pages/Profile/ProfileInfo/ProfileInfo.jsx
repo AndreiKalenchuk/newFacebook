0bf7b1986668ba613db486d149ce739b2c120432
KALIANCHUK, ANDREI <ak276p@us.att.com>
2021-09-12 21:50:14 -0700

    adopted local state setState for ProfileStatus.js class

--- src/components/pages/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/pages/Profile/ProfileInfo/ProfileInfo.jsx
@@ -3,6 +3,7 @@ import tree from '../../../../tree.jpg'
 import css from './ProfileInfo.module.css';
 import unKnownUserPhoto from "../../../../assets/imegess/unknownUser.png"
 import Preloader from "../../../common/preloaders/preloader";
+import ProfileStatus from "./ProfileStatus";
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
@@ -19,6 +20,7 @@ const ProfileInfo = (props) => {
                 <img src={props.profile.photos.large ? props.profile.photos.large : unKnownUserPhoto}/>
                 </span>
 
+                <ProfileStatus status='hello everyone!! '/>
                 <span>
                 <div> About me: {props.profile.aboutMe} </div>
                 <div>Name: {props.profile.fullName}</div>
