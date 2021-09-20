2b757200f8a3fba0e15a68e1bdbf03c5074cdf14
KALIANCHUK, ANDREI <ak276p@us.att.com>
2021-09-19 21:07:01 -0700

    redux form fields validation

--- src/components/pages/Profile/MyPosts/MyPosts.jsx
+++ src/components/pages/Profile/MyPosts/MyPosts.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import css from './MyPosts.module.css'
 import Post from "./Post/Post";
 import {Field, reduxForm} from "redux-form";
+import {maxLengthValidator, requiredField} from "../../../utils/formValidators/validators";
+import {Textarea} from "../../../common/FormsControls/FormControls";
+
+const maxLength10 = maxLengthValidator(10);
 
 const MyPosts = (props) => {
     const postsItems = props.posts.map(post => <Post key={post.id} message={post.msg} id={post.id}
                                                      likesCount={post.likesCount}
                                                      increaseLikeCount={props.increaseLikeCount}/>);
 
-    const onAddNewPost = ( formData) => {
-            props.addNewPost(formData.newPost);
+    const onAddNewPost = (formData) => {
+        props.addNewPost(formData.newPost);
     }
 
     return (
@@ -28,7 +32,10 @@ const MyPosts = (props) => {
 const AddPostForm = (props) => {
     return (
         <form onSubmit={props.handleSubmit}>
-            <Field component='textArea' name='newPost' placedholder='type a message'/>
+            <Field component={Textarea}
+                   name='newPost'
+                   placedholder='type a message'
+                   validate={[requiredField, maxLength10]}/>
             <div>
                 <button> Add post</button>
             </div>
